9a60eb9f220f085f4eba96d97635c8410be7bb5f
designdotdevanshu <design.devanshu@gmail.com>
2025-09-11 00:59:14 +0530

    feat: add OAuth environment variables for GitHub, Google, and Facebook

--- src/env.js
+++ src/env.js
@@ -11,6 +11,14 @@ export const env = createEnv({
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
+    GITHUB_CLIENT_ID: z.string().min(1, "GITHUB_CLIENT_ID is required"),
+    GITHUB_CLIENT_SECRET: z.string().min(1, "GITHUB_CLIENT_SECRET is required"),
+    GOOGLE_CLIENT_ID: z.string().min(1, "GOOGLE_CLIENT_ID is required"),
+    GOOGLE_CLIENT_SECRET: z.string().min(1, "GOOGLE_CLIENT_SECRET is required"),
+    FACEBOOK_CLIENT_ID: z.string().min(1, "FACEBOOK_CLIENT_ID is required"),
+    FACEBOOK_CLIENT_SECRET: z
+      .string()
+      .min(1, "FACEBOOK_CLIENT_SECRET is required"),
   },
 
   /**
@@ -29,7 +37,12 @@ export const env = createEnv({
   runtimeEnv: {
     DATABASE_URL: process.env.DATABASE_URL,
     NODE_ENV: process.env.NODE_ENV,
-    // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+    GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_ID,
+    GOOGLE_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GITHUB_CLIENT_ID,
+    FACEBOOK_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+    FACEBOOK_CLIENT_SECRET: process.env.GITHUB_CLIENT_ID,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
