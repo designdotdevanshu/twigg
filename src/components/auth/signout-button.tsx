fd1f96a43f694e9778c7a7f71b010c8ac098e5ca
designdotdevanshu <design.devanshu@gmail.com>
2025-10-10 00:54:31 +0530

    feat: enhance logout button styling for improved dark mode support

--- src/components/auth/signout-button.tsx
+++ src/components/auth/signout-button.tsx
@@ -12,10 +12,12 @@ export function LogoutButton() {
       size="lg"
       variant="ghost"
       onClick={handleLogout}
-      className="h-14 w-full items-center justify-start gap-5 rounded-none border-b border-gray-200 p-4 px-8 text-xs font-medium hover:bg-gray-100"
+      className="h-14 w-full items-center justify-start gap-5 rounded-none border-b border-gray-200 p-4 px-8 text-xs font-medium hover:bg-gray-100 dark:border-gray-700 dark:hover:bg-gray-800"
     >
-      <LogOutIcon className="h-2 w-2 text-gray-600" />
-      <span className="ttext-xs font-medium text-gray-700">Sign out</span>
+      <LogOutIcon className="h-2 w-2 text-gray-600 dark:text-gray-300" />
+      <span className="text-xs font-medium text-gray-700 dark:text-gray-300">
+        Sign out
+      </span>
     </Button>
   );
 }
